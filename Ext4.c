0c824daf0673c1230db729cd87cd2c2e82287306
mmanchon <ls30685@salleurl.edu>
2018-04-14 20:30:15 +0200

    #1 Leemos la información de ext4, haria falta revisarla y mirar lo de que el padding no lo podemos dar por supuesto

---
+++ Ext4.c
@@ -0,0 +1,5 @@
+//
+// Created by sergi on 14/4/18.
+//
+
+#include "Ext4.h"
---
+++ Ext4.h
@@ -0,0 +1,8 @@
+//
+// Created by sergi on 14/4/18.
+//
+
+#ifndef RAGNAROK_EXT4_H
+#define RAGNAROK_EXT4_H
+
+#endif //RAGNAROK_EXT4_H
